e0849ba5a8239368826997c42b88a2ed2d51977a
Roney <107938728+RoneyPrates@users.noreply.github.com>
2024-08-31 22:01:47 -0300

    Delete sync/sync directory

--- sync/sync/src/main/java/com/example/sync/repository/UsuariosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sync.repository;
-
-import com.example.sync.model.Usuarios;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
-    Usuarios findByEmail(String email);
-}
